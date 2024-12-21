bce0f6c2befcc22f948f91eebae5b2bc22aa09d0
gnanendraprasad <gnanendra097@gmail.com>
2024-12-21 14:21:48 -0600

    Exercise_1 complete
    Iterative Binary Search with time and space complexity included.

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,13 +1,20 @@
-# Python code to implement iterative Binary  
-# Search. 
+# Python code to implement iterative Binary Search. 
+
+#Time complexity: worst case - O(logn), best case: O(1) if the ele is in mid
+#Space complexity: O(1)
   
 # It returns location of x in given array arr  
 # if present, else returns -1 
 def binarySearch(arr, l, r, x): 
-  
-  #write your code here
-  
-    
+    while l<r:
+        mid = (l+r)//2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid]>x:
+            r = mid-1
+        else:
+            l = mid+1
+    return -1    
   
 # Test array 
 arr = [ 2, 3, 4, 10, 40 ] 
@@ -17,6 +24,6 @@ x = 10
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print(f"Element is present at index {result+1}") 
 else: 
-    print "Element is not present in array"
+    print("Element is not present in the array")
